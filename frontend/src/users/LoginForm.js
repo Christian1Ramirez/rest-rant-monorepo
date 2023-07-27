98ec7fb30e9e13d363350fca830dec3e3ea55c60
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-07-27 16:06:58 -0700

    saving before switching back to main

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,9 +17,9 @@ function LoginForm() {
 
   
     async function handleSubmit(e) {
-        e.preventDefault()
         const response = await fetch(`http://localhost:5000/authentication/`, {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
